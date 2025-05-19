14cf6c17540c75b97bccfd58d098211538e17306
HImanshu <124870650+HJimanshu@users.noreply.github.com>
2025-05-19 23:51:16 +0530

    Update settings.py

--- coding_editor/settings.py
+++ coding_editor/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vercel-backend-zcwr.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
