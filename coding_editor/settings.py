e5ad106ce77da183b063b3f8ffd6d5336e8cd0d4
HImanshu <124870650+HJimanshu@users.noreply.github.com>
2025-05-20 01:48:18 +0530

    Update settings.py

--- coding_editor/settings.py
+++ coding_editor/settings.py
@@ -90,6 +90,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+# ]
